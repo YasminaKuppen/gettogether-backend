b2efba0a82bbb5d1266ada125f24c81bb5145d16
YasminaKuppen <YasminaKuppen@hotmail.com>
2025-01-11 12:24:24 +0100

    entiteit weekend gemaakt

--- src/main/java/nl/novi/gettogetherbackend/models/Weekend.java
+++ src/main/java/nl/novi/gettogetherbackend/models/Weekend.java
@@ -1,4 +1,81 @@
 package nl.novi.gettogetherbackend.models;
 
+import java.util.Date;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+import java.util.List;
+
+@Entity
+@Table(name = "weekends")
+
 public class Weekend {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotNull(message = "Date cannot be null")
+    private Date date;
+    @NotNull(message = "Time cannot be null")
+    private String time;
+    @NotBlank(message = "Location cannot be empty")
+    private String location;
+
+    private int temperature;
+
+    @OneToOne
+    @JoinColumn(name = "group_id", referencedColumnName = "id")
+    private Group group;
+
+    public Weekend() {
+    }
+
+    public Weekend(Date date, String time, String location, int temperature) {
+        this.date = date;
+        this.time = time;
+        this.location = location;
+        this.temperature = temperature;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public int getTemperature() {
+        return temperature;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public void setTemperature(int temperature) {
+        this.temperature = temperature;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
 }
