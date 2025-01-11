efcfdc3a2028e86c957efe50eaf40d3b9b91c9e5
YasminaKuppen <YasminaKuppen@hotmail.com>
2025-01-11 11:47:19 +0100

    packages aangemaakt

---
+++ src/main/java/nl/novi/gettogetherbackend/models/User.java
@@ -0,0 +1,4 @@
+package nl.novi.gettogetherbackend.models;
+
+public class User {
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO public.activities(
+    year, brand, model)
+VALUES
+    (2011, 'toyota', 'yaris'),
+    (2017, 'alfa romeo', '156')
+;
+-- INSERT INTO users (username, password, role) VALUES ('admin', '$2a$10$MVPMSEUxcsALNlLQDaNjcO3KfNE3ySxVQUDvJhIvyEkCxG.gC7Gc.', 'ADMIN');
+-- INSERT INTO users (username, password, role) VALUES ('user', '$2a$10$MVPMSEUxcsALNlLQDaNjcO3KfNE3ySxVQUDvJhIvyEkCxG.gC7Gc.', 'USER');
