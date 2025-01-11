1c9a9c79e68652fd3e9e1fa81d9534727a8557d2
YasminaKuppen <YasminaKuppen@hotmail.com>
2025-01-11 11:03:23 +0100

    project opgestart

---
+++ src/main/java/nl/novi/gettogetherbackend/GettogetherBackendApplication.java
@@ -0,0 +1,13 @@
+package nl.novi.gettogetherbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GettogetherBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GettogetherBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/nl/novi/gettogetherbackend/GettogetherBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.novi.gettogetherbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GettogetherBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
